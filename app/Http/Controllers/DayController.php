054bed7588d4eea45efae9863aee05cdd24b1e1d
Jasper Briers <jasper@compubel.be>
2022-12-08 00:07:03 +0100

    Improve day view
    - Use Tailwind CSS
    - Add previous/next days pagination

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\File;
 
 class DayController extends Controller
 {
@@ -23,6 +24,17 @@ class DayController extends Controller
             'day' => $day,
             'part1' => $day_class->part1(),
             'part2' => $day_class->part2(),
+            'previous_day_link' => $this->getOtherDayLink($day, false),
+            'next_day_link' => $this->getOtherDayLink($day),
         ])->render();
+    }
+
+    private function getOtherDayLink(int $day, bool $next = true): ?string
+    {
+        $the_day = ($next) ? $day + 1 : $day - 1;
+
+        $path = base_path('app/Days').'/Day'.$the_day.'.php';
+
+        return File::exists($path) ? '/day/'.$the_day : null;
     }
 }
